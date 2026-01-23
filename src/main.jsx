46a2e16a798b5f66c704fa9b4e256925b1128eb0
sunnychauhan1924-sudo <sunnychauhan1924@gmail.com>
2026-01-23 18:19:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/auth.jsx";
 import { SearchProvider } from "./context/search.jsx";
 import { CartProvider } from "./context/cart.jsx";
+import axios from "axios";
+
+axios.defaults.baseURL =
+  import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
