901a02ba38ef58c0a609748fcadadf64f5d4f4ae
sunnychauhan1924-sudo <sunnychauhan1924@gmail.com>
2026-01-26 22:14:45 +0530

    Update AdminOrders.jsx

--- src/pages/Admin/AdminOrders.jsx
+++ src/pages/Admin/AdminOrders.jsx
@@ -91,7 +91,7 @@ const AdminOrders = () => {
                     <div className="row mb-2 p-3 card flex-row" key={p._id}>
                       <div className="col-md-4">
                         <img
-                          src={`/api/product/product-photo/${p._id}`}
+                          src={`${import.meta.env.VITE_API_URL}/api/product/product-photo/${p._id}`}
                           className="card-img-top"
                           alt={p.name}
                           width="100px"
